7b841dfe1ec397f682b21f26cb7d979f4a00e44c
Alex Pletzer <alexander@gokliya.net>
2018-12-13 16:37:54 +1300

    working on the bar plot

--- _lessons/python-scatter/images/summary.py
+++ _lessons/python-scatter/images/summary.py
@@ -1,16 +1,15 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
-
-xlabels =     ['original'  , 'vect' , 'numba', 'ctypes', 'openmp-c 8th', 'multiproc th'  , 'mpi4py 8pr'   ]
-times   =     [15*60+50.   , 2*60+6., 1*60+4., 26.     , 10.      ,  15*60+47.44, 2*60+14.   ]
-
-fig, ax = plt.subplots()
-
-ind = np.arange(len(xlabels))
-plt.bar(ind, times)
-ax.set_xticklabels(xlabels)
-
-ax.set_ylabel('Wall clock time [s]')
-ax.set_title('python scatter.py -nx 256 -ny 256 -nc 256')
-
+import matplotlib.pyplot as plt
+ 
+objects = ['original'  , 'vect' , 'numba', 'ctypes', 'multiproc 8th'  , 'mpi4py 8pr', 'openmp-c 8th'   ]
+y_pos = np.arange(len(objects))
+times_sec = [15*60+50.   , 2*60+6., 1*60+4., 26.     ,  3*60+34.98, 2*60+14., 10.         ]
+ 
+plt.bar(y_pos, times_sec, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+plt.ylabel('Wall clock time [s]')
+plt.title('Mahuika: python scatter.py -nx 256 -ny 256 -nc 256')
+ 
 plt.show()
+
